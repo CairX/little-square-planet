ec8b18841d8b59462f62bd2a84c8651681afc63b
Thomas Cairns <lazycairx@gmail.com>
2017-06-29 12:51:42 +0200

    Camera movement

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+	public float Speed;
+
+	private void Update() {
+		// Reverse becaues we are moving the camera and not an object.
+		if (Input.GetButton("Camera Up")) {
+			Move(Vector3.down);
+		}
+		else if (Input.GetButton("Camera Down")) {
+			Move(Vector3.up);
+		}
+		if (Input.GetButton("Camera Left")) {
+			Move(Vector3.right);
+		} else if (Input.GetButton("Camera Right")) {
+			Move(Vector3.left);
+		}
+	}
+
+	private void Move(Vector3 direction) {
+		transform.position = transform.position + direction * Speed * Time.deltaTime;
+	}
+}
